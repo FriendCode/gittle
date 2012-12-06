bc4ec94994c664c15b1180cbf12684af957e61e6
Aaron O'Mullan <aaron.omullan@friendco.de>
2012-12-05 16:30:23 -0800

    Add: File tracking ability

--- gittle/gittle.py
+++ gittle/gittle.py
@@ -1,3 +1,7 @@
+# Python imports
+import os
+import copy
+
 # Dulwich imports
 from dulwich.repo import Repo as DRepo
 from dulwich.client import get_transport_and_path
@@ -13,15 +17,40 @@ class Gittle(object):
         'email': None,
     }
 
+    HIDDEN_REGEXES = [
+        # Hide git directory
+        r'./.git/',
+    ]
+
     def __init__(self, repo_or_path):
         if isinstance(repo_or_path, DRepo):
             self.repo = repo_or_path
-            self.path = self.repo.path
         elif isinstance(repo_or_path, basestring):
-            self.path = repo_or_path
             self.repo = DRepo(repo_or_path)
         else:
             raise Exception('Gittle must be initialized with either a dulwich repository or a string to the path')
+
+        # Set path
+        self.path = self.repo.path
+
+        # Build ignore filter
+        self.hidden_regexes = copy.copy(self.HIDDEN_REGEXES)
+        self.hidden_regexes.extend(self._get_ignore_regexes())
+        self.ignore_filter = utils.path_filter_regex(self.hidden_regexes)
+        self.filters = [
+            self.ignore_filter,
+        ]
+
+    def _get_ignore_regexes(self):
+        gitignore_filename = os.path.join(self.path, '.gitignore')
+        lines = open(gitignore_filename).readlines()
+        globers = map(lambda line: line.rstrip(), lines)
+        return utils.globers_to_regex(globers)
+
+    @property
+    @utils.memoize
+    def index(self):
+        return self.repo.open_index()
 
     @classmethod
     def init(cls, path):
@@ -79,6 +108,31 @@ class Gittle(object):
     def commit_files(self):
         pass
 
+    @property
+    @utils.transform(set)
+    def tracked_files(self):
+        return self.index._byname.keys()
+
+    @property
+    @utils.transform(set)
+    def raw_files(self):
+        return utils.subpaths(self.path)
+
+    @property
+    @utils.transform(set)
+    def ignored_files(self):
+        return utils.subpaths(self.path, filters=self.filters)
+
+    @property
+    @utils.transform(set)
+    def trackable_files(self):
+        return self.raw_files - self.ignored_files
+
+    @property
+    @utils.transform(set)
+    def untracked_files(self):
+        return self.trackable_files - self.tracked_files
+
     # Like: git add
     @utils.arglist_method
     def add(self, files):
@@ -87,7 +141,10 @@ class Gittle(object):
     # Like: git rm
     @utils.arglist_method
     def rm(self, files, force=False):
-        pass
+        for f in files:
+            if not f in self.index:
+                continue
+            del self.index[f]
 
     # Like: git mv
     @utils.arglist_method
@@ -102,7 +159,6 @@ class Gittle(object):
     def reset(self, files):
         pass
 
-    def reset_all(self):
-        index = self.repo.open_index()
-        index.clear()
-        return index.write()
+    def rm_all(self):
+        self.index.clear()
+        return self.index.write()
--- gittle/utils.py
+++ gittle/utils.py
@@ -1,4 +1,8 @@
 # Python imports
+import os
+import re
+import fnmatch
+import operator
 from functools import wraps
 
 # Constants
@@ -53,10 +57,131 @@ def arglist(func):
 def arglist_method(func):
     @wraps(func)
     def f(self, *args, **kwargs):
-        print('ARGS', args)
         args_list = list_from_args(args)
-        print('ARGS LIST', args_list)
         return func(self, args_list, **kwargs)
     return f
 
-# Some useless comment
+
+# Cache calls
+def memoize(func):
+    """Cache a functions output for a given set of arguments"""
+    cache = {}
+
+    @wraps(func)
+    def f(*args, **kwargs):
+        sorted_kwargs = kwargs.items()
+        sorted_kwargs.sort()
+        cache_key = hash(args + tuple(sorted_kwargs))
+        if cache_key in cache:
+            return cache[cache_key]
+        cache[cache_key] = func(*args, **kwargs)
+        return cache[cache_key]
+    return f
+
+
+def transform(transform_func):
+    """Apply a transformation to a functions return value"""
+    def decorator(func):
+        @wraps(func)
+        def f(*args, **kwargs):
+            return transform_func(
+                func(*args, **kwargs)
+            )
+        return f
+    return decorator
+
+
+# Useful functions
+negate = transform(operator.not_)
+
+
+# Path filters
+def path_filter_hidden(path, abspath):
+    return path.startswith('.')
+
+path_filter_visible = negate(path_filter_hidden)
+
+
+def path_filter_directory(path, abspath):
+    return os.path.isdir(abspath)
+
+path_filter_file = negate(path_filter_directory)
+
+
+@arglist
+def path_filter_regex(regexes):
+    compiled_regexes = map(re.compile, regexes)
+
+    def _filter(path, abspath):
+        return any([
+            cre.match(abspath)
+            for cre in compiled_regexes
+        ])
+    return _filter
+
+
+@arglist
+def combine_filters(filters):
+    def combined_filter(path, abspath):
+        filter_results = [
+            _filter(path, abspath)
+            for _filter in filters
+        ]
+        return all(filter_results)
+    return combined_filter
+
+
+def abspaths_only(paths_couple):
+    return map(lambda x: x[1], paths_couple)
+
+
+def clean_relative_paths(paths):
+    return [
+        p[2:]
+        for p in paths
+    ]
+
+
+def dir_subpaths(root_path):
+    """Get paths in a given directory"""
+    paths = []
+    for dirname, dirnames, filenames in os.walk(root_path):
+
+        # Add directory paths
+        paths.extend([
+            # path, abspath
+            (subdirname, os.path.join(dirname, subdirname))
+            for subdirname in dirnames
+        ])
+
+        # Add file paths
+        paths.extend([
+            # path, abspath
+            (filename, os.path.join(dirname, filename))
+            for filename in filenames
+        ])
+    return paths
+
+
+def subpaths(root_path, filters=None):
+    if filters is None:
+        filters = [
+            path_filter_visible,
+            path_filter_file,
+        ]
+
+    # One big filter which combines all other smaller filters
+    big_filter = combine_filters(filters)
+    filter_func = lambda x: big_filter(x[0], x[1])
+
+    paths = dir_subpaths(root_path)
+
+    # Do filtering
+    filtered_paths = filter(filter_func, paths)
+    abs_filtered_paths = abspaths_only(filtered_paths)
+    return clean_relative_paths(abs_filtered_paths)
+
+
+@arglist
+def globers_to_regex(globers):
+    return map(fnmatch.translate, globers)
