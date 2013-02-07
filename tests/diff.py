c0f9c0fb139ece1e90cb08b4bf51a0f428375deb
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-02-07 13:23:42 +0100

    Fix: Reverse SHA order in diff test

--- tests/diff.py
+++ tests/diff.py
@@ -2,9 +2,9 @@ from gittle import Gittle
 
 repo = Gittle('.')
 
-lastest = [
+lastest = reversed([
     info['sha']
     for info in repo.commit_info()[:2]
-]
+])
 
 print(repo.diff_between(*lastest))
