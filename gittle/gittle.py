6f2824eda333716f4449045a3fdb17739769897f
gsw945 <gsw945@users.noreply.github.com>
2015-12-11 21:29:20 +0800

    Update gittle.py
    fixed error in Python 3

--- gittle/gittle.py
+++ gittle/gittle.py
@@ -95,7 +95,7 @@ class Gittle(object):
     PATTERN_MODIFIED = (True, True)
 
     # Permissions
-    MODE_DIRECTORY = 040000  # Used to tell if a tree entry is a directory
+    MODE_DIRECTORY = 0o40000  # Used to tell if a tree entry is a directory
 
     # Tree depth
     MAX_TREE_DEPTH = 1000
